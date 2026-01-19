7c0893947f9f6cb7949c0b0b15a32d3253ade179
mollymcl <mollymclaughlin263@gmail.com>
2026-01-19 11:06:58 -0600

    Forgot to edit routing to routeroutlet

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
