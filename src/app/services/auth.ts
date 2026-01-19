0ac4aeb79b37de3771ce103334c5ca891d108234
mollymcl <mollymclaughlin263@gmail.com>
2026-01-19 11:18:05 -0600

    Now can see password input box, added but not linking auth/mailer under services folder

---
+++ src/app/services/auth.ts
@@ -0,0 +1,47 @@
+import { Injectable, inject, signal} from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost:8080';
+  
+  router = inject(Router);
+  loggedIn = new BehaviorSubject<boolean>(false);
+  redirectUrl: string | null = null;
+
+  constructor(private http:HttpClient){}
+
+  login(password:string): Observable<any>{
+    return this.http.post(`${this.apiUrl}/login`, {password: password}, {withCredentials:true}).pipe(
+      tap(() => {
+        this.loggedIn.next(true);
+      })
+    );
+  }
+
+  logout(): void{
+    this.redirectUrl = null;
+    this.http.post(`${this.apiUrl}/logout`, {}, {withCredentials:true}).subscribe(() => this.loggedIn.next(false));
+  }
+
+  isAuthenticated(): Observable<any>{
+    return this.http.get(`${this.apiUrl}/verify`, {withCredentials:true}).pipe(
+      map(() => {
+        this.loggedIn.next(true);
+        return true;
+    }),
+    catchError(() => {
+      this.loggedIn.next(false);
+      return of(false);
+    })
+  );
+  }
+
+
+}
