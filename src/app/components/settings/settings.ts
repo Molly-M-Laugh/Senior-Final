c96fdd06947a6ac2921f37be07dd5479cd322873
mollymcl <mollymclaughlin263@gmail.com>
2026-02-02 21:49:06 -0600

    Added base html, css. Will have to watch content on bootstrap css and html videos to get more modern view

--- src/app/components/settings/settings.ts
+++ src/app/components/settings/settings.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,ReactiveFormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
 export class Settings {
+  optionsForm:FormGroup;
+  router = inject(Router);
 
+  constructor(private fb:FormBuilder){
+    this.optionsForm=this.fb.group({});
+  }
+
+  home(){
+    this.router.navigateByUrl("/home")
+  }
 }
