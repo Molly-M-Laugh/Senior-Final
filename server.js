7ec859b8d7274e9de03518075368e2ea8e40e0f9
colin-sampey <colinsampey@gmail.com>
2026-03-28 15:35:01 -0500

    adding test for status code feedback in auth

--- server.js
+++ server.js
@@ -163,6 +163,9 @@ app.post('/api/login', async (req, res) => {
     // Later, have check if no value returned
     const isMatch = result.rows[0]?.is_match || false;
     res.json({is_match : isMatch});
+    if(isMatch == false){
+      res.statusCode = 401
+    }
   } catch (err) {
     console.error(err);
   }
